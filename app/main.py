5f38dc7ee236c055567807af58ca3376f22df50b
fatinsajid <fatinsajid@gmail.com>
2026-03-30 00:17:14 +0600

    Latest Commit

--- app/main.py
+++ app/main.py
@@ -44,7 +44,10 @@ app = FastAPI(title="Shariah Compliance API - Supabase Refactored")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/", "https://shariahcompliance.onrender.com"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://shariahcompliance.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
